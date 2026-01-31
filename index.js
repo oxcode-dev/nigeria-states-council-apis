1c7b95af613712aa0fa4e14dc7272c5b2b12ceda
Oxcode <mrexcelsam1@gmail.com>
2026-01-31 18:36:20 +0100

    initial commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import statesRouter from "./routes/statesRouter.js"
 // import lgasRouter from "./routes/lgasRouter.js"
 // import wardsRouter from "./routes/wardsRouter.js"
 import bodyParser from "body-parser";
+import { State } from "./models/State.js";
 
 config();
 
