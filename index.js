31bd5eb51bb8b02a21b655bd07a875bf3a33c090
Oxcode <mrexcelsam1@gmail.com>
2026-01-31 19:00:11 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import express from "express"
 import mongoose from "mongoose"
 import { config } from "dotenv"
-// import statesRouter from "./routes/statesRouter.js"
-// import lgasRouter from "./routes/lgasRouter.js"
-// import wardsRouter from "./routes/wardsRouter.js"
+import statesRouter from "./routes/statesRouter.js"
+import lgasRouter from "./routes/lgasRouter.js"
+import wardsRouter from "./routes/wardsRouter.js"
 import bodyParser from "body-parser";
-import { State } from "./models/State.js";
 
 config();
 
@@ -38,21 +37,6 @@ mongoose
 
 app.use(express.json());
 
-app.get('/api', async(req, res) => {
-
-    try {
-        // return res.status(201).send('Hello World');
-
-        const states = await State.find();
-
-        return res.status(201).send(states);
-    }
-    catch (error) {
-        console.log(error)
-        res.status(500).send({ message: error.message })
-    }
-})
-
-// app.use('/api/states', statesRouter)
-// app.use('/api/lgas', lgasRouter)
-// app.use('/api/wards', wardsRouter)
+app.use('/api/states', statesRouter)
+app.use('/api/lgas', lgasRouter)
+app.use('/api/wards', wardsRouter)
