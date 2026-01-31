552372dd2273ba2fa2b1fe07858dcce64f64e0c7
Oxcode <mrexcelsam1@gmail.com>
2026-01-31 18:29:04 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express, { json } from "express"
-import mongoose from "mongoose"
+// import mongoose from "mongoose"
 import { config } from "dotenv"
-import statesRouter from "./routes/statesRouter.js"
-import lgasRouter from "./routes/lgasRouter.js"
-import wardsRouter from "./routes/wardsRouter.js"
+// import statesRouter from "./routes/statesRouter.js"
+// import lgasRouter from "./routes/lgasRouter.js"
+// import wardsRouter from "./routes/wardsRouter.js"
 import bodyParser from "body-parser";
 
 config();
@@ -24,19 +24,19 @@ app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
 
-mongoose
-  .connect(process.env.mongodb, {
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true,
-      // useCreateIndex: true,
-      serverSelectionTimeoutMS: 30000,
-      socketTimeoutMS: 45000,
-  })
-  .then(() => console.log('Database is connected'))
-  .catch((error) => console.error('Database connection error', error))
+// mongoose
+//   .connect(process.env.mongodb, {
+//       // useNewUrlParser: true,
+//       // useUnifiedTopology: true,
+//       // useCreateIndex: true,
+//       serverSelectionTimeoutMS: 30000,
+//       socketTimeoutMS: 45000,
+//   })
+//   .then(() => console.log('Database is connected'))
+//   .catch((error) => console.error('Database connection error', error))
 
-app.use(express.json());
+// app.use(express.json());
 
-app.use('/api/states', statesRouter)
-app.use('/api/lgas', lgasRouter)
-app.use('/api/wards', wardsRouter)
+// app.use('/api/states', statesRouter)
+// app.use('/api/lgas', lgasRouter)
+// app.use('/api/wards', wardsRouter)
