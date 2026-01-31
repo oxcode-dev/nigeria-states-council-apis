9d460fdc6c7314335e089f646599b9c0f6c26e2f
Oxcode <mrexcelsam1@gmail.com>
2026-01-31 19:06:59 +0100

    initial commit

--- routes/statesRouter.js
+++ routes/statesRouter.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/', async(req, res) => {
 
     try {
-        return res.status(201).send('Hello World');
+        // return res.status(201).send('Hello World');
 
         const states = await State.find();
 
@@ -18,109 +18,109 @@ router.get('/', async(req, res) => {
     }
 })
 
-// router.post('/', async (request, response) => {
-//     try {
-//         const formData = request.body;
-//         if(
-//             !formData.name || !formData.code || 
-//             !formData.zone || !formData.slogan
-//         ) {
-//             return response.status(400).send({
-//                 message: "Required fields are missing!",
-//             })
-//         }
+router.post('/', async (request, response) => {
+    try {
+        const formData = request.body;
+        if(
+            !formData.name || !formData.code || 
+            !formData.zone || !formData.slogan
+        ) {
+            return response.status(400).send({
+                message: "Required fields are missing!",
+            })
+        }
 
-//         const newState = {
-//             name: formData.name,
-//             bio: formData.bio,
-//             code: formData.code,
-//             slogan: formData.slogan,
-//             zone: formData.zone,
-//         }
+        const newState = {
+            name: formData.name,
+            bio: formData.bio,
+            code: formData.code,
+            slogan: formData.slogan,
+            zone: formData.zone,
+        }
 
-//         const state = await State.create(newState);
+        const state = await State.create(newState);
 
-//         return response.status(201).send(state);
-//     }
-//     catch (error) {
-//         console.log(error)
-//         response.status(500).send({ message: error.message })
-//     }
-// })
+        return response.status(201).send(state);
+    }
+    catch (error) {
+        console.log(error)
+        response.status(500).send({ message: error.message })
+    }
+})
 
-// router.get('/:id', async (request, response) => {
-//     try {
-//         const { id } = request.params;
+router.get('/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
 
-//         const state = await State.findById(id);
+        const state = await State.findById(id);
 
-//         return response.status(201).send(state);
-//     }
-//     catch (error) {
-//         console.log(error)
-//         response.status(500).send({ message: error.message })
-//     }
-// })
+        return response.status(201).send(state);
+    }
+    catch (error) {
+        console.log(error)
+        response.status(500).send({ message: error.message })
+    }
+})
 
-// router.delete('/:id', async (request, response) => {
-//     try {
-//         const { id } = request.params;
+router.delete('/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
 
-//         const result = await State.findByIdAndDelete(id);
+        const result = await State.findByIdAndDelete(id);
 
-//         if(!result) {
-//             return response.status(404).send({
-//                 message: "State not found!",
-//             })
-//         }
+        if(!result) {
+            return response.status(404).send({
+                message: "State not found!",
+            })
+        }
 
-//         return response.status(201).send({
-//             message: 'State successfully deleted!!!',
-//             deletedItem: result,
-//         });
-//     }
-//     catch (error) {
-//         console.log(error)
-//         response.status(500).send({ message: error.message })
-//     }
-// })
+        return response.status(201).send({
+            message: 'State successfully deleted!!!',
+            deletedItem: result,
+        });
+    }
+    catch (error) {
+        console.log(error)
+        response.status(500).send({ message: error.message })
+    }
+})
 
-// router.put('/:id', async (request, response) => {
-//     try {
-//         const { id } = request.params;
+router.put('/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
 
-//         const formData = request.body;
-//         if(
-//             !formData.name || !formData.code || 
-//             !formData.zone || !formData.slogan
-//         ) {
-//             return response.status(400).send({
-//                 message: "Required fields are missing!",
-//             })
-//         }
+        const formData = request.body;
+        if(
+            !formData.name || !formData.code || 
+            !formData.zone || !formData.slogan
+        ) {
+            return response.status(400).send({
+                message: "Required fields are missing!",
+            })
+        }
 
-//         const updatedState = {
-//             name: formData.name,
-//             bio: formData.bio,
-//             code: formData.code,
-//             slogan: formData.slogan,
-//             zone: formData.zone,
-//         }
+        const updatedState = {
+            name: formData.name,
+            bio: formData.bio,
+            code: formData.code,
+            slogan: formData.slogan,
+            zone: formData.zone,
+        }
 
-//         const result = await State.findByIdAndUpdate(id, updatedState, { new: true});
+        const result = await State.findByIdAndUpdate(id, updatedState, { new: true});
 
-//         if(!result) {
-//             return response.status(404).send({
-//                 message: "State not found!",
-//             })
-//         }
+        if(!result) {
+            return response.status(404).send({
+                message: "State not found!",
+            })
+        }
 
-//         return response.status(201).send(result);
-//     }
-//     catch (error) {
-//         console.log(error)
-//         response.status(500).send({ message: error.message })
-//     }
-// })
+        return response.status(201).send(result);
+    }
+    catch (error) {
+        console.log(error)
+        response.status(500).send({ message: error.message })
+    }
+})
 
 export default router;
