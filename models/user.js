838a14be9450d4a082e98d7966760c64f8e849e5
Oxcode <mrexcelsam1@gmail.com>
2026-02-01 19:22:36 +0100

    auth middleware

--- models/user.js
+++ models/user.js
@@ -9,6 +9,14 @@ const userSchema = mongoose.Schema({
     password: {
         type: String,
         required: true,
+    },
+    first_name: {
+        type: String,
+        required: true,
+    },
+    last_name: {
+        type: String,
+        required: true, 
     }
 });
 
