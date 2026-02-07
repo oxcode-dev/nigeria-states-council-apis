89909e4a8cc5a6427711cc8286debfdc082536fa
Oxcode <mrexcelsam1@gmail.com>
2026-02-07 13:34:49 +0100

    initial commit

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -6,7 +6,12 @@ import { User } from '../models/user.js';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    
+    try {
+        
+        return res.status(200).json({ message: 'my details' })
+    } catch(error) {
+        return res.status(500).json({ message: 'server error'})
+    }
 })
 
 export { router as profileRouter };
