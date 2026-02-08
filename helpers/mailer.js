1a8cc4b97d959071cddb06cf4a2656e7053bf4f2
Oxcode <mrexcelsam1@gmail.com>
2026-02-08 13:10:38 +0100

    initial commit

---
+++ helpers/mailer.js
@@ -0,0 +1,36 @@
+import nodemailer from 'nodemailer';
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+    service: "gmail",
+	// host: process.env.EMAIL_SMTP_HOST,
+	// port: process.env.EMAIL_SMTP_PORT,
+	//secure: process.env.EMAIL_SMTP_SECURE, // lack of ssl commented this. You can uncomment it.
+	auth: {
+		user: process.env.EMAIL_SMTP_USERNAME,
+		pass: process.env.EMAIL_SMTP_PASSWORD
+	}
+});
+// const mailOptions = {
+//     from: "your.email@gmail.com",    // Sender address (Gmail overwrites this with the authenticated user)
+//     to: "recipient.email@example.com", // List of receivers, separated by commas
+//     subject: "Sending Email using Node.js", // Subject line
+//     text: "Hello world?",             // plain text body
+//     html: "<b>Hello world?</b>",      // html body
+// };
+
+
+const sendMail = function (from, to, subject, html)
+{
+	// send mail with defined transport object
+	// visit https://nodemailer.com/ for more options
+	return transporter.sendMail({
+		from: from, // sender address e.g. no-reply@xyz.com or "Fred Foo 👻" <foo@example.com>
+		to: to, // list of receivers e.g. bar@example.com, baz@example.com
+		subject: subject, // Subject line e.g. 'Hello ✔'
+		//text: text, // plain text body e.g. Hello world?
+		html: html // html body e.g. '<b>Hello world?</b>'
+	});
+};
+
+export { sendMail };
