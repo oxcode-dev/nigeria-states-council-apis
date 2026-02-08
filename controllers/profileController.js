8b93a23e78f7e9e70b55cd2085a03b04b60a7b98
Oxcode <mrexcelsam1@gmail.com>
2026-02-08 10:01:07 +0100

    initial commit

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -30,4 +30,47 @@ router.get('/', auth, async (req, res) => {
     }
 })
 
+router.put('/', auth, async (req, res) => {
+    try {
+        const auth = req.user
+        const user = await User.findById(auth.id)
+
+        if(!user) {
+            return res.status(404).json({ message: 'User not found' })
+        }
+
+        const { first_name, last_name, email } = req.body;
+
+        if(
+            !first_name || !last_name || !email
+        ) {
+            return res.status(400).json({
+                message: "Required fields are missing!",
+            })
+        }
+
+        if(first_name) user.first_name = first_name;
+        if(last_name) user.last_name = last_name;
+        if(email) user.email = email;
+
+        await user.save();
+
+        let data = {
+            status: "success",
+            message: "Profile updated successfully",
+            user: {
+                id: user._id,
+                fullName: user.fullName,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                first_name: user.first_name,
+                last_name: user.last_name,
+            },
+        };
+        res.status(200).json(data);
+    } catch(error) {
+        return res.status(500).json({ message: 'server error'})
+    }
+})
+
 export { router as profileRouter };
