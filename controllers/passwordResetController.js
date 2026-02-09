6888dff5e0c5c915a37098b8767f01bc153d5090
Oxcode <mrexcelsam1@gmail.com>
2026-02-10 00:18:13 +0100

    initial commit

---
+++ controllers/passwordResetController.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { User } from '../models/user.js';
+
+const router = express.Router();
+
+// Router for user login
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if(!user || !isMatch) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+
+        const payload = { 
+            id: user._id,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        }
+        jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            { expiresIn: 3600*24},
+            (error, token) => {
+                if(error) throw error;
+
+                res.json({
+                    token,
+                    user: {
+                        id: user._id,
+                        email: user.email,
+                        first_name: user.first_name,
+                        last_name: user.last_name
+                    },
+                    message: 'Login successful'
+                })
+            }
+        );
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({ message: 'Server error' });
+    }
+})
+
+export { router as authRouter };
