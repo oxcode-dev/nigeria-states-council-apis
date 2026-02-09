dc1c8d1dcb1278fe8da7c084a93bcbbe62a39eb5
Oxcode <mrexcelsam1@gmail.com>
2026-02-09 17:31:03 +0100

    initial commit

--- middlewares/adminMiddleware.js
+++ middlewares/adminMiddleware.js
@@ -1,6 +1,5 @@
 
 const adminMiddleware = (req, res, next) => {
-    // Assuming authMiddleware has run and attached req.user
     if (!req.user || !req.user.isAdmin) {
         return res.status(403).json({ error: 'Access denied. Admin privileges required.' });
     }
