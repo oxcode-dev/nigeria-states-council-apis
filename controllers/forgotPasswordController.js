8e1db7618fb85b09ea9daffc6cdce3063a6f13d9
Oxcode <mrexcelsam1@gmail.com>
2026-02-10 00:21:58 +0100

    initial commit

--- controllers/forgotPasswordController.js
+++ controllers/forgotPasswordController.js
@@ -8,38 +8,21 @@ const router = express.Router();
 // Router for user login
 router.post('/forgot-password', async (req, res) => {
     try {
-        const { email, password } = req.body;
+        const { email } = req.body;
         const user = await User.findOne({ email });
-        const isMatch = await bcrypt.compare(password, user.password)
 
-        if(!user || !isMatch) {
+        if(!user) {
             return res.status(400).json({ message: 'Invalid credentials' });
         }
 
-        const payload = { 
-            id: user._id,
-            email: user.email,
-            isAdmin: user.isAdmin,
-        }
-        jwt.sign(
-            payload,
-            process.env.JWT_SECRET,
-            { expiresIn: 3600*24},
-            (error, token) => {
-                if(error) throw error;
-
-                res.json({
-                    token,
-                    user: {
-                        id: user._id,
-                        email: user.email,
-                        first_name: user.first_name,
-                        last_name: user.last_name
-                    },
-                    message: 'Login successful'
-                })
-            }
+        await sendMail(
+            process.env.EMAIL_SMTP_USERNAME,
+            result.email,
+            // 'mrexcelsam1@gmail.com',
+            "Account Deletion Notification",
+            `<p>Your account with email ${result.email} has been deleted.</p>`
         );
+        
     } catch (error) {
         console.error(error)
         return res.status(500).json({ message: 'Server error' });
