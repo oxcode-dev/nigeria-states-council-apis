0c692f3a7517a9e7e2fb5f071df20f9e48fdd0cc
Oxcode <mrexcelsam1@gmail.com>
2026-02-15 21:39:15 +0100

    initial commit

--- routes/publicStateRouter.js
+++ routes/publicStateRouter.js
@@ -85,36 +85,4 @@ router.get('/states-with-lgas', async(req, res) => {
     }
 })
 
-router.get('/lgas/', async(req, res) => {
-
-    try {
-        const lgas = await LocalGovt.find().exec();
-
-        const totalCount = await LocalGovt.countDocuments();
-
-        return res.status(201).send({
-            data: lgas,
-            metadata: {
-                totalCount,
-            }
-        });
-    }
-    catch (error) {
-        res.status(500).send({ message: error.message })
-    }
-})
-
-router.get('/lgas/:id', async (request, response) => {
-    try {
-        const { id } = request.params;
-
-        const lga = await LocalGovt.findById(id);
-
-        return response.status(201).send(lga);
-    }
-    catch (error) {
-        response.status(500).send({ message: error.message })
-    }
-})
-
 export default router;
